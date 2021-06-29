8925da41496caea7cc776a9d8a89e98de2834b1f
yamaguchi371 <telework-takefu6@outlook.jp>
2021-06-29 11:48:38 +0900

    memo.vue作成まで

---
+++ pages/memo.vue
@@ -0,0 +1,14 @@
+<template>
+    <section class="container">
+        <h1>Memo</h1>
+        <table>
+            <tr>
+                <th>Title</th>
+                <td><input type="text" name="title"
+                class="title" size="40"
+                v-model="title" @focus="set_flg">
+                <button @click="find">find</button></td>
+            </tr>
+        </table>
+    </section>
+</template>
